fa9f88bc7231e82bf3691d5620ff5b777ac7ab16
kajal4497 <76652014+kajal4497@users.noreply.github.com>
2021-10-03 18:50:29 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,12 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6 imgs
 var randomDiceImage = "dice" + randomNumber1 + ".png";
-var randomImageSource = "images/" +randomDiceImage; // images from 1-6
+var randomImageSource = randomDiceImage; // images from 1-6
 var image1 = document.querySelectorAll("img")[0];  //based on image tag
 image1.setAttribute("src", randomImageSource); //attribute name that we want to change: src
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src" , randomImageSource2);
  
 // if player 1 wins
